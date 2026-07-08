d9ec07a24c5853340f980cac898c15a4fbeb6f48
Prittam <prittamraj2018282@gmail.com>
2026-07-08 14:08:38 +0530

    API for Initial fund transfer by System created

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -12,4 +12,11 @@ const transactionRoutes = Router()
 transactionRoutes.post("/", authMiddleware.authMiddleware, transactionController.createTransaction)
 
 
-module.exports = transactionRoutes
+/**
+ * - POST /api/transactions/system/initial-funds
+ * - Create initial funds transaction from system user
+ */
+transactionRoutes.post("/system/initial-funds", authMiddleware.authSystemUserMiddleware, transactionController.createInitialFundsTransaction)
+
+
+module.exports = transactionRoutes;
